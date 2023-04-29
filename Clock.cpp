07886feebac3c01b7c67337a40fb43f2993d8f25
federicomarra <fecrom@gmail.com>
2023-04-29 04:38:35 +0200

    Compressed change language & changed viewMode 0

--- Clock.cpp
+++ Clock.cpp
@@ -42,145 +42,78 @@ string Clock::getDate() const {
             temp = to_string(date.tm_mday);
             s += "-" + (temp.length() == 1 ? ('0' + temp) : temp);
             break;
-        case 2:   // wday dd mmm yyyy
-            if (ita) {
-                switch (date.tm_wday) {
-                    case 0:
-                        s += "Dom";
-                        break;
-                    case 1:
-                        s += "Lun";
-                        break;
-                    case 2:
-                        s += "Mar";
-                        break;
-                    case 3:
-                        s += "Mer";
-                        break;
-                    case 4:
-                        s += "Gio";
-                        break;
-                    case 5:
-                        s += "Ven";
-                        break;
-                    case 6:
-                        s += "Sab";
-                        break;
-                }
-                temp = to_string(date.tm_mday);
-                s += ' ' + (temp.length() == 1 ? ('0' + temp) : temp) + ' ';
-                switch (date.tm_mon) {
-                    case 0:
-                        s += "Gen";
-                        break;
-                    case 1:
-                        s += "Feb";
-                        break;
-                    case 2:
-                        s += "Mar";
-                        break;
-                    case 3:
-                        s += "Apr";
-                        break;
-                    case 4:
-                        s += "Mag";
-                        break;
-                    case 5:
-                        s += "Giu";
-                        break;
-                    case 6:
-                        s += "Lug";
-                        break;
-                    case 7:
-                        s += "Ago";
-                        break;
-                    case 8:
-                        s += "Set";
-                        break;
-                    case 9:
-                        s += "Ott";
-                        break;
-                    case 10:
-                        s += "Nov";
-                        break;
-                    case 11:
-                        s += "Dic";
-                        break;
-                }
-                s += ' ' + to_string(1900 + date.tm_year);
-            } else {
-                switch (date.tm_wday) {
-                    case 0:
-                        s += "Sun";
-                        break;
-                    case 1:
-                        s += "Mon";
-                        break;
-                    case 2:
-                        s += "Tue";
-                        break;
-                    case 3:
-                        s += "Wed";
-                        break;
-                    case 4:
-                        s += "Thu";
-                        break;
-                    case 5:
-                        s += "Fri";
-                        break;
-                    case 6:
-                        s += "Sat";
-                        break;
-                }
-                temp = to_string(date.tm_mday);
-                s += ' ' + (temp.length() == 1 ? ('0' + temp) : temp) + ' ';
-                switch (date.tm_mon) {
-                    case 0:
-                        s += "Jan";
-                        break;
-                    case 1:
-                        s += "Feb";
-                        break;
-                    case 2:
-                        s += "Mar";
-                        break;
-                    case 3:
-                        s += "Apr";
-                        break;
-                    case 4:
-                        s += "May";
-                        break;
-                    case 5:
-                        s += "Jun";
-                        break;
-                    case 6:
-                        s += "Jul";
-                        break;
-                    case 7:
-                        s += "Aug";
-                        break;
-                    case 8:
-                        s += "Sep";
-                        break;
-                    case 9:
-                        s += "Oct";
-                        break;
-                    case 10:
-                        s += "Nov";
-                        break;
-                    case 11:
-                        s += "Dec";
-                        break;
-                }
-                s += ' ' + to_string(1900 + date.tm_year);
+        case 2:   // wdd dd mmm yyyy
+            switch (date.tm_wday) {     // week day
+                case 0:
+                    s += (ita ? "Dom" : "Sun");
+                    break;
+                case 1:
+                    s += (ita ? "Lun" : "Mon");
+                    break;
+                case 2:
+                    s += (ita ? "Mar" : "Tue");
+                    break;
+                case 3:
+                    s += (ita ? "Mer" : "Wed");
+                    break;
+                case 4:
+                    s += (ita ? "Gio" : "Thu");
+                    break;
+                case 5:
+                    s += (ita ? "Ven" : "Fri");
+                    break;
+                case 6:
+                    s += (ita ? "Sab" : "Sat");
+                    break;
             }
-            break;
-        default:    // dd/mm/'yy
             temp = to_string(date.tm_mday);
-            s += temp.length() == 1 ? ('0' + temp) : temp;
+            s += ' ' + (temp.length() == 1 ? ('0' + temp) : temp) + ' ';
+            switch (date.tm_mon) {      // month
+                case 0:
+                    s += (ita ? "Gen" : "Jan");
+                    break;
+                case 1:
+                    s += "Feb";
+                    break;
+                case 2:
+                    s += "Mar";
+                    break;
+                case 3:
+                    s += "Apr";
+                    break;
+                case 4:
+                    s += (ita ? "Mag" : "May");
+                    break;
+                case 5:
+                    s += (ita ? "Giu" : "Jun");
+                    break;
+                case 6:
+                    s += (ita ? "Lug" : "Jul");
+                    break;
+                case 7:
+                    s += (ita ? "Ago" : "Aug");
+                    break;
+                case 8:
+                    s += (ita ? "Set" : "Sep");
+                    break;
+                case 9:
+                    s += (ita ? "Ott" : "Oct");
+                    break;
+                case 10:
+                    s += "Nov";
+                    break;
+                case 11:
+                    s += (ita ? "Dic" : "Dec");
+                    break;
+            }
+            s += ' ' + to_string(1900 + date.tm_year);
+            break;
+        default:    // dd/mm/yyyy
+            temp = to_string(date.tm_mday);
+            s += (temp.length() == 1 ? ('0' + temp) : temp);
             temp = to_string(date.tm_mon + 1);
             s += "/" + (temp.length() == 1 ? ('0' + temp) : temp);
-            s += "/'" + to_string(date.tm_year % 100);
+            s += "/" + to_string(1900 + date.tm_year);
     }
     return s;
 }
